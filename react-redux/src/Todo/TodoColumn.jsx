ae532520a02e55f8f68fb8d4c0d98fb471e95d9b
Максим Водянов <max.post@bk.ru>
2020-08-20 18:51:38 +0300

    refactoring

--- react-redux/src/Todo/TodoColumn.jsx
+++ react-redux/src/Todo/TodoColumn.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 class TodoColumn extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     const { name, items } = this.props;
