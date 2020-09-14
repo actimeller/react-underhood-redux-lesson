81f25c36b3dde6a27cd15c9361d3ca21548ff798
Максим Водянов <max.post@bk.ru>
2020-09-14 22:54:25 +0300

    small refactoring

--- react-redux/src/utils/normalize.js
+++ react-redux/src/utils/normalize.js
@@ -2,13 +2,18 @@ const normalize = (input, schema) => {
   if (typeof input !== "object" || !input) {
     return input;
   }
-  
+
+  if (Array.isArray(input)) {
+    input.forEach((item) => normalize(item, schema));
+  }
+
   const object = { ...input };
   Object.keys(schema).forEach((key) => {
     const value = normalize(input[key], schema[key]);
     if (value === undefined || value === null) {
       delete object[key];
     } else {
+        console.info(234);
       object[key] = value;
     }
   });
